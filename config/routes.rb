1292c8e10855506ed5d6d7320026bdf401929a28
Percy <edoumps@gmail.com>
2022-12-06 12:22:53 +0100

    Created a Store model,the catalog index page & styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # root "products#index"
+  root 'store#index', as: 'store_index'
+  get 'store/index'
   resources :products
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "products#index"
 end
